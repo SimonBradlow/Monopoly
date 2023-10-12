df699862c81ba79d7c39e02b6ee79f36f428e8fe
bmbowman2000 <bmbowman@uvm.edu>
2023-10-12 10:50:45 -0400

    Created init function in square.py

--- square.py
+++ square.py
@@ -1,9 +1,14 @@
+import property.py
+
 class Square():
     """
     The class representing an individual square on the board
     """
-    def __init__():
-        pass
+    def __init__(self, name: str, property: Property, x: int, y: int):
+        self.name = name
+        self.property = property
+        self.x = x
+        self.y = y
 
     def draw():
         pass
