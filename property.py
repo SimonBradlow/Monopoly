ada37f3b759b15ee6584b10b2bc8175142a3b58d
Will <William.Shayne@uvm.edu>
2023-10-11 23:54:32 -0400

    Added basic fields to property class

--- property.py
+++ property.py
@@ -2,8 +2,15 @@ class Property():
     """
     Class representing properties in Monopoly
     """
-    def __init__():
-        pass
+    def __init__(self, name: str, price: int, rents: list, building_cost: int, mortgage_value: int, mortgaged: bool = False, building_count: int = 0):
+        self.name = name
+        self.price = price
+        self.rents = rents
+        self.building_cost = building_cost
+        self.mortgage_value = mortgage_value
+        self.mortgaged = mortgaged
+        self.building_count = building_count
 
-    def draw():
+
+    def draw(self):
         pass
