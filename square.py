24b793fe6f8f20d551705ca4db43e92bb0328543
Simon Bradlow <simon@bradlow.com>
2023-10-16 20:42:36 -0400

    initial corner sprite funcitonality

--- square.py
+++ square.py
@@ -10,16 +10,28 @@ class Square():
         self.property = property
         self.x = x
         self.y = y
+        self.width = width
+        self.height = height
 
         # CODE BELOW IS NEEDED FOR Square.draw() METHOD
         # Initialize shape_list
         self.shape_list = arcade.ShapeElementList()
+        self.sprite_list = arcade.SpriteList()
 
         # Create shape objects
-        background = arcade.create_rectangle_filled(0, 0, width, height, arcade.color.MINT) 
-        border = arcade.create_rectangle_outline(0, 0, width, height, arcade.color.BLACK, 2) 
+        background = arcade.create_rectangle_filled(0, 0, width, height, (204, 227, 199)) 
+        border = arcade.create_rectangle_outline(0, 0, width, height, arcade.color.BLACK, 1) 
         inner = arcade.create_rectangle_filled(0, (height/8)*3, width, height/4, arcade.color.BLUE)
-        inner_outline = arcade.create_rectangle_outline(0, (height/8)*3, width, height/4, arcade.color.BLACK, 2)
+        inner_outline = arcade.create_rectangle_outline(0, (height/8)*3, width, height/4, arcade.color.BLACK, 1)
+
+        # add Sprites
+        # TODO: select specific sprite img based on property.name
+        # TODO: remove unecessary shapes from tiles with sprites
+        scaling = width/204
+        tile_sprite = arcade.Sprite("assets/go.png", scaling)
+        tile_sprite.center_x = 0
+        tile_sprite.center_y = 0
+        self.sprite_list.append(tile_sprite)
 
         # Add objects to the shape_list
         self.shape_list.append(background)
@@ -35,3 +47,12 @@ class Square():
 
         # Draw the shape_list
         self.shape_list.draw()
+
+        # Corner pieces
+        # if self.property.name == "go"
+        if self.width == self.height:
+            for s in self.sprite_list:
+                s.center_x = x
+                s.center_y = y
+                s.angle = angle
+            self.sprite_list.draw()
