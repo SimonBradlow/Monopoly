4e47a3cc29c37a2102b2d72e13753f0397f5ff85
Simon Bradlow <simon@bradlow.com>
2023-10-25 19:31:23 -0400

    refactored window to allow for multiple views

--- window.py
+++ window.py
@@ -6,18 +6,18 @@ SCREEN_HEIGHT = 800
 EDGE_SPACE = 100
 SCREEN_TITLE = "Monopoly!"
 
-class MyGame(arcade.Window):
+# MAIN GAME VIEW
+class GameView(arcade.View):
     """
-    Main application class.
+    Main game/board class.
 
     NOTE: Go ahead and delete the methods you don't need.
     If you do need a method, delete the 'pass' and replace it
     with your own code. Don't leave 'pass' in this program.
     """
 
-    def __init__(self, width, height, title):
-        super().__init__(width, height, title)
-
+    def __init__(self):
+        super().__init__()
         arcade.set_background_color(arcade.color.AMAZON)
 
         # If you have sprite lists, you should create them here,
@@ -47,6 +47,7 @@ class MyGame(arcade.Window):
         Normally, you'll call update() on the sprite lists that
         need it.
         """
+        # This is where you would check the win condition for GameOverView()
         pass
 
     def on_key_press(self, key, key_modifiers):
@@ -82,11 +83,55 @@ class MyGame(arcade.Window):
         """
         pass
 
+# START SCREEN VIEW
+class StartView(arcade.View):
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.AMAZON)
+
+    def on_draw(self):
+        self.clear()
+        arcade.draw_text("Welcome to Monopoly!", SCREEN_WIDTH/2, SCREEN_HEIGHT/2,
+                         arcade.color.WHITE_SMOKE, font_size=40, anchor_x="center")
+        arcade.draw_text("Click to advance", SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - 75,
+                         arcade.color.GRAY, font_size=20, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        board_view = GameView()
+        board_view.setup()
+        self.window.show_view(board_view)
+
+# PROPERTY CARD VIEW(?)
+class PropertyView(arcade.View):
+    def on_show_view(self):
+        pass
+
+# GAME OVER SCREEN VIEW
+class GameOverView(arcade.View):
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_draw(self):
+        self.clear()
+        """
+        Draw "Game over" across the screen.
+        """
+        arcade.draw_text("Game Over", self.width/2 - 200, 400, arcade.color.WHITE, 54)
+        arcade.draw_text("U suck!", self.width/2 - 50, 300, arcade.color.WHITE, 24)
+
+        time_taken_formatted = f"{round(self.time_taken, 2)} seconds"
+        arcade.draw_text(f"Time taken: {time_taken_formatted}",
+                         self.width / 2,
+                         200,
+                         arcade.color.GRAY,
+                         font_size=15,
+                         anchor_x="center")
+
 
 def main():
     """ Main function """
-    game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    game.setup()
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    start_view = StartView()
+    window.show_view(start_view)
     arcade.run()
 
 
