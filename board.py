f940b12b35773838176edf93417211b88b779450
Simon Bradlow <simon@bradlow.com>
2023-10-25 16:49:57 -0400

    moved window functionality out of board.py

--- board.py
+++ board.py
@@ -1,47 +1,20 @@
 import arcade
 from square import Square
 from property import Property
-from gameOverView import GameOverView
 import csv
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
-EDGE_SPACE = 100
-SCREEN_TITLE = "Monopoly!"
-
-class Board(arcade.Window):
-    """
-    Main application class.
-
-    NOTE: Go ahead and delete the methods you don't need.
-    If you do need a method, delete the 'pass' and replace it
-    with your own code. Don't leave 'pass' in this program.
-    """
-
-    def __init__(self):
-        super().__init__()
-
-        self.width = 600
-        self.height = 600
-        arcade.set_background_color(arcade.color.AMAZON)
-
-
-        # If you have sprite lists, you should create them here,
-        # and set them to None
-
-    def on_show_view(self):
-        arcade.set_background_color(arcade.color.AMAZON)
-
-    def setup(self):
-        """ Set up the game variables. Call to re-start the game. """
-        # Create your sprites and sprite lists here
-        #initialize squares list
+class Board():
+    def __init__(self, w, h, e):
+        # Initialize squares list
         self.squares = []
+        self.SCREEN_WIDTH = w
+        self.SCREEN_HEIGHT = h
+        self.EDGE_SPACE = e
         with open('board.csv', mode ='r') as board_file:
             boardreader = csv.DictReader(board_file)
             for line in boardreader:
-                width = ((SCREEN_WIDTH-(EDGE_SPACE*2))*(3/4))/9
-                height = (SCREEN_HEIGHT-(EDGE_SPACE*2))/8
+                width = ((self.SCREEN_WIDTH-(self.EDGE_SPACE*2))*(3/4))/9
+                height = (self.SCREEN_HEIGHT-(self.EDGE_SPACE*2))/8
                 p = None
                 # Create the property from the CSV file (the None values are for mortgage and building cost, which aren't in the CSV yet)
                 corner_names = ['Go', 'Jail', 'Parking', 'GoToJail']
@@ -56,37 +29,36 @@ class Board(arcade.Window):
                 # Add the square to the list of squares, was unsure what to initialize x/y/height/width to
                 self.squares.append(Square(int(line['Position']), p, width, height))
 
-    def on_draw(self):
-        self.clear()
+    def draw(self):
         board_color = arcade.color.BLACK
-        board_center_x = SCREEN_WIDTH/2
-        board_center_y = SCREEN_HEIGHT/2
-        inner_board_width = (SCREEN_WIDTH-(EDGE_SPACE*2))*(3/4)
-        inner_board_height = (SCREEN_HEIGHT-(EDGE_SPACE*2))*(3/4)
-        outer_board_width = SCREEN_WIDTH-(EDGE_SPACE*2)
-        outer_board_height = SCREEN_HEIGHT-(EDGE_SPACE*2)
+        board_center_x = self.SCREEN_WIDTH/2
+        board_center_y = self.SCREEN_HEIGHT/2
+        inner_board_width = (self.SCREEN_WIDTH-(self.EDGE_SPACE*2))*(3/4)
+        inner_board_height = (self.SCREEN_HEIGHT-(self.EDGE_SPACE*2))*(3/4)
+        outer_board_width = self.SCREEN_WIDTH-(self.EDGE_SPACE*2)
+        outer_board_height = self.SCREEN_HEIGHT-(self.EDGE_SPACE*2)
         border_width = 1
         
         tile_width = inner_board_width/9
         tile_height = outer_board_height/8
         num_tiles = 9
         
-        row_tile_x = EDGE_SPACE+tile_height+(tile_width/2)
-        bottom_tile_y = EDGE_SPACE+tile_height/2
-        top_tile_y = SCREEN_HEIGHT-EDGE_SPACE-(tile_height/2)
-        column_tile_y = EDGE_SPACE+tile_height+(tile_width/2)
-        left_tile_x = EDGE_SPACE+tile_height/2
-        right_tile_x = SCREEN_WIDTH-EDGE_SPACE-(tile_height/2)
+        row_tile_x = self.EDGE_SPACE+tile_height+(tile_width/2)
+        bottom_tile_y = self.EDGE_SPACE+tile_height/2
+        top_tile_y = self.SCREEN_HEIGHT-self.EDGE_SPACE-(tile_height/2)
+        column_tile_y = self.EDGE_SPACE+tile_height+(tile_width/2)
+        left_tile_x = self.EDGE_SPACE+tile_height/2
+        right_tile_x = self.SCREEN_WIDTH-self.EDGE_SPACE-(tile_height/2)
         
         bottom_tile_tilt = 0
         right_tile_tilt = 90
         top_tile_tilt = 180
         left_tile_tilt = 270
 
-        left_corners_x = EDGE_SPACE+tile_height/2
-        bottom_corners_y = EDGE_SPACE+tile_height/2
-        right_corners_x = SCREEN_WIDTH-EDGE_SPACE-(tile_height/2)
-        top_corners_y = SCREEN_HEIGHT-EDGE_SPACE-(tile_height/2)
+        left_corners_x = self.EDGE_SPACE+tile_height/2
+        bottom_corners_y = self.EDGE_SPACE+tile_height/2
+        right_corners_x = self.SCREEN_WIDTH-self.EDGE_SPACE-(tile_height/2)
+        top_corners_y = self.SCREEN_HEIGHT-self.EDGE_SPACE-(tile_height/2)
 
         #draw inner square of board
         arcade.draw_rectangle_filled(board_center_x, board_center_y, 
@@ -97,7 +69,8 @@ class Board(arcade.Window):
         logo = arcade.load_texture("assets/logo.png")
         logo_scale = (inner_board_width*(3/4))/600
         logo_tilt_angle = -45
-        arcade.draw_scaled_texture_rectangle(self.width/2, self.height/2,
+        arcade.draw_scaled_texture_rectangle(self.SCREEN_WIDTH/2, 
+                                             self.SCREEN_HEIGHT/2,
                                              logo, logo_scale, logo_tilt_angle)
 
         #draw outer square of board
@@ -106,16 +79,16 @@ class Board(arcade.Window):
                                       board_color, border_width)
 
         #draw community chest
-        arcade.draw_rectangle_outline((SCREEN_WIDTH/2)+(inner_board_width/4),
-                                      (SCREEN_HEIGHT/2)+(inner_board_height/4),
+        arcade.draw_rectangle_outline((self.SCREEN_WIDTH/2)+(inner_board_width/4),
+                                      (self.SCREEN_HEIGHT/2)+(inner_board_height/4),
                                       tile_width, tile_width*2, 
                                       board_color, 
                                       border_width, 
                                       logo_tilt_angle - 90)
 
         #draw chance
-        arcade.draw_rectangle_outline((SCREEN_WIDTH/2)-(inner_board_width/4), 
-                                      (SCREEN_HEIGHT/2)-(inner_board_height/4), 
+        arcade.draw_rectangle_outline((self.SCREEN_WIDTH/2)-(inner_board_width/4), 
+                                      (self.SCREEN_HEIGHT/2)-(inner_board_height/4), 
                                       tile_width, tile_width*2, 
                                       board_color, 
                                       border_width, 
@@ -124,7 +97,7 @@ class Board(arcade.Window):
         property = Property("test", "test", 0, [], 0, 0)
 
         #draw go tile at position 0
-        self.squares[0].draw(left_corners_x, bottom_corners_y, corner_tile_tilt)
+        self.squares[0].draw(left_corners_x, bottom_corners_y, bottom_tile_tilt)
 
         # call square for each tile in left column
         for i in range(1, num_tiles + 1):
@@ -133,7 +106,7 @@ class Board(arcade.Window):
             column_tile_y += tile_width
 
         #draw jail tile at position 10
-        self.squares[10].draw(left_corners_x, top_corners_y, corner_tile_tilt)
+        self.squares[10].draw(left_corners_x, top_corners_y, bottom_tile_tilt)
 
         # call square for each tile in top row
         for i in range(11, num_tiles + 11):
@@ -142,10 +115,10 @@ class Board(arcade.Window):
             row_tile_x += tile_width
 
         #draw free parking tile at position 20
-        self.squares[20].draw(right_corners_x, top_corners_y, corner_tile_tilt)
+        self.squares[20].draw(right_corners_x, top_corners_y, bottom_tile_tilt)
 
         #reset column y
-        column_tile_y = SCREEN_HEIGHT-EDGE_SPACE-(tile_height+(tile_width/2))
+        column_tile_y = self.SCREEN_HEIGHT-self.EDGE_SPACE-(tile_height+(tile_width/2))
 
         # call square for each tile in right column
         for i in range(21, num_tiles + 21):
@@ -154,10 +127,10 @@ class Board(arcade.Window):
             column_tile_y -= tile_width
 
         #draw go to jail at position 30
-        self.squares[30].draw(right_corners_x, bottom_corners_y, corner_tile_tilt)
+        self.squares[30].draw(right_corners_x, bottom_corners_y, bottom_tile_tilt)
 
         #reset row x
-        row_tile_x = SCREEN_WIDTH-EDGE_SPACE-(tile_height+(tile_width/2))
+        row_tile_x = self.SCREEN_WIDTH-self.EDGE_SPACE-(tile_height+(tile_width/2))
 
         #call square for each tile in bottom row
         for i in range(31, num_tiles + 31):
@@ -165,53 +138,3 @@ class Board(arcade.Window):
             self.squares[i].draw(row_tile_x, bottom_tile_y, bottom_tile_tilt)
 
             row_tile_x -= tile_width
-
-
-
-
-
-    def on_update(self, delta_time):
-        """
-        All the logic to move, and the game logic goes here.
-        Normally, you'll call update() on the sprite lists that
-        need it.
-        """
-        pass
-
-    def on_key_press(self, key, key_modifiers):
-        """
-        Called whenever a key on the keyboard is pressed.
-
-
-
-        For a full list of keys, see:
-        https://api.arcade.academy/en/latest/arcade.key.html
-        """
-
-        if key == 65307:
-            board_view = GameOverView()
-            self.window.show_view(board_view)
-
-    def on_key_release(self, key, key_modifiers):
-        """
-        Called whenever the user lets off a previously pressed key.
-        """
-        pass
-
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
-        """
-        Called whenever the mouse moves.
-        """
-        pass
-
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
-
-    def on_mouse_release(self, x, y, button, key_modifiers):
-        """
-        Called when a user releases a mouse button.
-        """
-        pass
