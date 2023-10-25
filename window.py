a80825573dd394c884f89700affa888a4ccac451
Simon Bradlow <simon@bradlow.com>
2023-10-25 19:40:48 -0400

    Moved non-main views into separate file

--- window.py
+++ window.py
@@ -1,5 +1,8 @@
 import arcade
 from board import Board
+from views import StartView
+from views import PropertyView
+from views import GameOverView
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
@@ -83,55 +86,12 @@ class GameView(arcade.View):
         """
         pass
 
-# START SCREEN VIEW
-class StartView(arcade.View):
-    def on_show_view(self):
-        arcade.set_background_color(arcade.color.AMAZON)
-
-    def on_draw(self):
-        self.clear()
-        arcade.draw_text("Welcome to Monopoly!", SCREEN_WIDTH/2, SCREEN_HEIGHT/2,
-                         arcade.color.WHITE_SMOKE, font_size=40, anchor_x="center")
-        arcade.draw_text("Click to advance", SCREEN_WIDTH/2, SCREEN_HEIGHT/2 - 75,
-                         arcade.color.GRAY, font_size=20, anchor_x="center")
-
-    def on_mouse_press(self, _x, _y, _button, _modifiers):
-        board_view = GameView()
-        board_view.setup()
-        self.window.show_view(board_view)
-
-# PROPERTY CARD VIEW(?)
-class PropertyView(arcade.View):
-    def on_show_view(self):
-        pass
-
-# GAME OVER SCREEN VIEW
-class GameOverView(arcade.View):
-    def on_show_view(self):
-        arcade.set_background_color(arcade.color.BLACK)
-
-    def on_draw(self):
-        self.clear()
-        """
-        Draw "Game over" across the screen.
-        """
-        arcade.draw_text("Game Over", self.width/2 - 200, 400, arcade.color.WHITE, 54)
-        arcade.draw_text("U suck!", self.width/2 - 50, 300, arcade.color.WHITE, 24)
-
-        time_taken_formatted = f"{round(self.time_taken, 2)} seconds"
-        arcade.draw_text(f"Time taken: {time_taken_formatted}",
-                         self.width / 2,
-                         200,
-                         arcade.color.GRAY,
-                         font_size=15,
-                         anchor_x="center")
-
 
 def main():
     """ Main function """
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    start_view = StartView()
-    window.show_view(start_view)
+    game_view = GameView()
+    window.show_view(game_view)
     arcade.run()
 
 
