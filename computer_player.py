997429bc9024dc53f2d935eae09a2bff46f67979
Will <William.Shayne@uvm.edu>
2023-11-20 23:38:09 -0500

    Added basic take_turn to ComputerPlayer

--- computer_player.py
+++ computer_player.py
@@ -25,10 +25,42 @@ class ComputerPlayer(Player):
         done = False
 
         while not done:
-            if "roll_move" in game.legal_actions()[0]:
-                pass
-            elif "roll_jail" in game.legal_actions()[0]:
-                pass
+            required_actions, other_actions, stubs = game.legal_actions()
+            if "roll_move" in required_actions:
+                game.roll_move()
+            elif "roll_jail" in required_actions:
+                if self.should_pay_fine():
+                    game.pay_fine()
+                else:
+                    game.roll_jail()
+            if "pay_rent" in required_actions:
+                game.pay_rent()
+            elif "draw_card" in required_actions:
+                game.draw_card()
+            elif "pay_tax" in required_actions:
+                game.pay_tax()
+            if "buy_property" in other_actions:
+                if self.should_buy(game.active_property, game):
+                    game.buy_property(game.active_property, self)
+            self.buy_upgrades(game)
+            if "end_turn" in game.legal_actions()[0]:
+                done = True
+                game.end_turn()
+        return True
+    
+    def buy_upgrades(self, game: Game):
+        """
+        buy_upgrades goes through all the properties this ComputerPlayer owns, buying upgrades until
+        should_upgrade tells it not to
+        """
+        bought = True
+
+        while bought:
+            bought = False
+            for prop in self.properties:
+                if self.should_upgrade(prop, game):
+                    game.buy_house(prop, self)
+                    bought = True
     
     def should_buy(self, property: Property, game: Game):
         """
@@ -47,3 +79,11 @@ class ComputerPlayer(Player):
         if not game.can_buy_house(property, self) or self.money < property.building_cost:
             return False
         return property.building_count < 4
+    
+    def should_pay_fine(self, game: Game):
+        """
+        should_pay_fine determines if the ComputerPlayer should pay their fine to get out of jail
+        """
+        if self.money < 50:
+            return False
+        return random.random() > 0.5
